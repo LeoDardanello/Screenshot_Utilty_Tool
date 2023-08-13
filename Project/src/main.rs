c959034f5c295ed458cf39e5559f2a7b4562b5e6
Leo <dardanelloleonardo@gmail.com>
2023-08-13 16:17:00 +0200

    added auto-incremental defautl name when saving

--- Project/src/main.rs
+++ Project/src/main.rs
@@ -10,7 +10,8 @@ pub struct MyApp {
     mode: i32,
     take_screen: bool,
     start_screen: bool,
-    image: Vec<screenshots::Image>
+    image: Vec<screenshots::Image>,
+    default_name:i32
 }
 
 impl MyApp {
@@ -22,14 +23,15 @@ impl MyApp {
         h.push(ToString::to_string("Take Screenshot: Ctrl+K"));
         h.push(ToString::to_string("Save: Maiusc+C+U"));
         h.push(ToString::to_string("Boh: LOLOLOLOLOL JOJOOOOOO"));
-
+        let initial_default_name=String::from("screenshot0");
         MyApp {
             hotkey_conf: HotkeysConfig::new(),
             output_format: default_output_format,
             mode: 0,
             take_screen: false,
             start_screen: false,
-            image: Vec::new()
+            image: Vec::new(),
+            default_name:0
         }
     }
 }
